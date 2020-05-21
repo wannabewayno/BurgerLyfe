b57f3a53096345cc2f15f819b16490712d52d3b8
Wayne <wayne.c.griffiths@gmail.com>
2020-05-22 01:44:12 +0800

    added media queries for mobile responsiveness

--- public/assets/javascript/lib/filling.js
+++ public/assets/javascript/lib/filling.js
@@ -1,12 +1,11 @@
-
+// loops through each class of 'filling' and sets some custom properties
+// this makes the page feel more alive.
 const setFillings = () => {
     $('.filling').each( function(index) {
-        //this is a test for hsl colours
-        const color = $('.title').css('background-color');
-        console.log(color);
         //the jQuery object of each filling
         const filling = $( this );
-        //gives a random width
+
+        // sets custom dynamic properties for this filling
         const width             = randomiseProperty(filling, { property:'width' ,adjustment:0.1, });
         const radius            = randomiseProperty(filling, { property:'border-radius', adjustment:3, });
         const backgroundColor   = colourPicker(filling);
@@ -18,17 +17,12 @@ const setFillings = () => {
             'backgroundColor':backgroundColor,
             'color':textColour
         }
-
+        //finally, add the markup
         $( filling ).css(CSS)
     });
 } 
 
-
-//TODO: will need to scale burger sizes so long words don't go out of the max-width
-
-//TODO: devoured mask will have bites out of it, by overlaying circles with the same background colour as the body. 
-//TODO: this mask will also have a text over lay (on an angle 30degree, like a red stamp called Devoured!)
-
+ 
 
 //TODO: re-size the input to make longer
 //TODO: change input colours for more impact
